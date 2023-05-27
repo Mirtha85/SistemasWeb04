f061c354e3b32a6e80cc37c89fd4d148667c9527
brayanGE <gildresespadabrayan@gmail.com>
2023-05-27 00:30:00 -0400

    agregue un action en home

---
+++ Migrations/20230527042826_agrege_un_action_para_home.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemasWeb01.Migrations
+{
+    public partial class agrege_un_action_para_home : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
