f80783411a1cb9e7a188b1e0b7c52c0dbcd7dd9a
brayanGE <gildresespadabrayan@gmail.com>
2023-05-28 12:48:12 -0400

    arregle vista product

---
+++ Migrations/20230528164655_arregle_vista_de_producto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemasWeb01.Migrations
+{
+    public partial class arregle_vista_de_producto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
