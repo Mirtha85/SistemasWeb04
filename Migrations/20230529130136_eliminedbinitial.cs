89db93461d590a6e312020b379ad0ea43c69775d
brayanGE <gildresespadabrayan@gmail.com>
2023-05-29 09:07:00 -0400

    elimine dbinitial

---
+++ Migrations/20230529130136_eliminedbinitial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemasWeb01.Migrations
+{
+    public partial class eliminedbinitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
