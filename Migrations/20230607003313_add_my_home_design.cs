f54c7a9a0036456ae81dbfd631a0b31972744a48
brayanGE <gildresespadabrayan@gmail.com>
2023-06-06 20:34:59 -0400

    agregue nuevo diseño en mi inicio de pantalla

---
+++ Migrations/20230607003313_add_my_home_design.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemasWeb01.Migrations
+{
+    public partial class add_my_home_design : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
